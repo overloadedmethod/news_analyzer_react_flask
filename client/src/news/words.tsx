ce3d50b32b4df9bca6c457a2b9e109d27e217e3e
overloadedmethod <overloadedmethod@gmail.com>
2020-10-18 00:54:42 +0300

    Added words representation

--- client/src/news/words.tsx
+++ client/src/news/words.tsx
@@ -2,13 +2,45 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 import NewsContext from "./context";
 
-const WordsBox = styled.div``;
+const WordsBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: start;
+  align-items: auto;
+  align-content: start;
+`;
+
+const WordBox = styled.div`
+  margin: 1rem;
+  box-shadow: -1px 3px 12px 1px rgba(143, 180, 186, 1);
+`;
+
+const Word = styled.span`
+  background-color: #acd7ec;
+  border-radius: 0.5rem 0 0 0.5rem;
+  padding: 0.5rem;
+`;
+
+const Amount = styled.span`
+  border-radius: 0 0.5rem 0.5rem 0;
+  background-color: #84dcc6;
+  height: 100%;
+  padding: 0.5rem;
+`;
 
 export const Words = () => {
   const [{ words }] = useContext(NewsContext);
 
   return (
-    <WordsBox>{Object.entries(words).map((word, amount) => word)}</WordsBox>
+    <WordsBox>
+      {Object.entries(words).map((word, amount) => (
+        <WordBox key={word}>
+          <Word>{word}</Word>
+          <Amount>{amount}</Amount>
+        </WordBox>
+      ))}
+    </WordsBox>
   );
 };
 
